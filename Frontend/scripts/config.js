f6ed4a4bdb0f9e899ae0c482a97af256e4dc2d85
iamsmsr <sm.saidur.rahman@g.bracu.ac.bd>
2025-09-19 17:22:54 +0600

    connected to azure container app

--- Frontend/scripts/config.js
+++ Frontend/scripts/config.js
@@ -5,7 +5,7 @@
 
 (function(){
     if (!window.API_BASE) {
-        window.API_BASE = 'https://plannraisyncb.onrender.com';
+        window.API_BASE = 'https://plannraisyncbackend.ambitiousflower-691abe83.centralindia.azurecontainerapps.io';
     }
     // Provide a convenience constant available in modules/scripts that don't read window
     try { window.API_BASE_STRING = window.API_BASE; } catch(e) { /* ignore */ }
